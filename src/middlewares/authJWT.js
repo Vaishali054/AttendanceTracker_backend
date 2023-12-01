218655923a736dc420fde3fdcd6d0958f167bc68
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-12-01 14:38:55 +0530

    adding middleware to authenticate JWT

---
+++ src/middlewares/authJWT.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+exports.authenticateJWT = async (req, res, next) => {
+  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : '';
+
+  if (!token) {
+    res.status(403).json({ message: 'Unauthorized' });
+    return;
+  }
+
+  try {
+    if (!process.env.JWT_AUTH_SECRET) {
+      throw new Error('JWT_AUTH_SECRET environment variable is not defined.');
+    }
+
+    let data;
+
+    try {
+      data = jwt.verify(token, process.env.JWT_AUTH_SECRET);
+    } catch (err) {
+      res.status(403).json({ message: 'Unauthorized' });
+      return;
+    }
+
+    if (typeof data === 'string') throw new Error('Invalid jwt data');
+
+    req.body.user = {
+      id: data.id,
+      role: data.role,
+    };
+    next();
+  } catch (err) {
+    res.status(500).json({
+      message: 'Internal server error during jwt authentication',
+    });
+    console.log(err);
+  }
+};
+
+
