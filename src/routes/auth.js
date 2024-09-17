d654a110c134e736b6de8de36deca3fe76901318
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-09-17 23:28:29 +0530

    modify: authrouter

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,23 +1,16 @@
 const { Router } = require('express');
 const { handleUserLogin, handleUserSignup } = require('../controllers/user');
-const { handleAdminLogin, handleAdminSignup } = require('../controllers/admin');
+const { handleAdminLogin} = require('../controllers/admin');
 const { authenticateJWT } = require('../middlewares/authJWT');
 const {getUser}=require("../controllers/details")
 
 
 const AuthRouter = Router();
 
-AuthRouter.post('/signup', (req, res) => {
-  const { role } = req.query;
-
-  if (role === 'user') handleUserSignup(req, res);
-  else if (role === 'admin') handleAdminSignup(req, res);
-  else res.status(404).json({ message: 'Role is not defined' });
-});
+AuthRouter.post('/signup', handleUserSignup);
 
 AuthRouter.post('/login', (req, res) => {
   const { role } = req.query;
-
 
   if (role === 'user') handleUserLogin(req, res);
   else if (role === 'admin') handleAdminLogin(req, res);
