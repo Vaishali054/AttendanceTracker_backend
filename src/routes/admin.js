c6107703f4be2df33621fd77646ef8d8bf3732dc
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-09-20 23:38:15 +0530

    add routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { authenticateJWT } = require('../middlewares/authJWT');
 const {addDegree, getDegree, editDegree, addDepartment, getDepartments
-    , editDepartment
+    , editDepartment, addSemester, getSemester, deleteSemester
 }=require('../controllers/admin')
 
 const AdminRouter=Router();
@@ -12,5 +12,8 @@ AdminRouter.post("/editdegree",authenticateJWT,editDegree)
 AdminRouter.post("/department/add",authenticateJWT,addDepartment)
 AdminRouter.get("/department/get",authenticateJWT,getDepartments)
 AdminRouter.post("/department/edit",authenticateJWT,editDepartment)
+AdminRouter.post("/semester/add",authenticateJWT,addSemester)
+AdminRouter.get("/semester/get",authenticateJWT,getSemester)
+AdminRouter.post("/semester/delete",authenticateJWT,deleteSemester)
 
 module.exports = AdminRouter;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const { handleUserLogin, handleUserSignup } = require('../controllers/user');
 const { handleAdminLogin} = require('../controllers/admin');
 const { authenticateJWT } = require('../middlewares/authJWT');
 const {getUser}=require("../controllers/details")
+const {sendOTP, verifyOTP}=require("../controllers/sendOTP")
 
 
 const AuthRouter = Router();
@@ -16,6 +17,9 @@ AuthRouter.post('/login', (req, res) => {
   else if (role === 'admin') handleAdminLogin(req, res);
   else res.status(404).json({ message: 'Role is not defined' });
 });
+
+AuthRouter.post('/sendOTP', sendOTP);
+AuthRouter.post('/verifyOTP', verifyOTP);
 
 AuthRouter.get("/me", authenticateJWT,getUser);
 
